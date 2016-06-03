5fc8e441b65f49a3861f0b58ac411529056569d1
SocratesDz <sdsxd1800@gmail.com>
2016-06-03 11:25:25 -0400

    Agregados los layouts para la lista y un adapter.

--- app/src/main/java/com/app/sunshine/socratesdiaz/sunshine/MainActivity.java
+++ app/src/main/java/com/app/sunshine/socratesdiaz/sunshine/MainActivity.java
@@ -2,6 +2,10 @@ package com.app.sunshine.socratesdiaz.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> fakeData = new ArrayList<String>();
+        fakeData.add("Today - Sunny - 88 / 63");
+        fakeData.add("Tomorrow - Foggy - 70 / 46");
+        fakeData.add("Weds - Cloudy - 72 / 63");
+        fakeData.add("Thurs - Rainy - 64 / 51");
+        fakeData.add("Fri - Foggy - 70 / 46");
+        fakeData.add("Sat - Sunny - 76 / 83");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.list_item_forecast, R.id.list_item_forecast_textview, fakeData);
+
+        ListView listView = (ListView) findViewById(R.id.listview_forecast);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
